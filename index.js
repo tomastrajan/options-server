5c2a8f2023f239048aa2c730cb938ab6b5d1878c
tomastrajan <tomas.trajan@gmail.com>
2017-07-11 14:53:29 +0200

    feat: add express server and chains request handler

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const fetch = require('node-fetch');
+
+const API_URL = 'https://query2.finance.yahoo.com/v7/finance/options/';
+
+const CORS_WHITELIST = [
+  'https://options-54580.firebaseapp.com',
+  'http://localhost'
+];
+const corsOptions = {
+  origin(origin, callback) {
+    CORS_WHITELIST.indexOf(origin) !== -1
+      ? callback(null, true)
+      : callback(new Error('Not allowed by CORS'))
+  }
+};
+
+const app = express();
+app.set('port', (process.env.PORT || 5000));
+app.use(cors(corsOptions));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (request, response) => response.send('Works!'));
+app.get('/chains', (request, response) => {
+  const { symbol, date } = request.query;
+  fetch(`${API_URL}${symbol}${date ? '?date=' + date : ''}`)
+    .then(res => response.send(res.json()));
+});
+
+app.listen(app.get('port'), () =>
+  console.log('Node app is running on port', app.get('port')));
